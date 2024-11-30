f0b32dfc829b1fb7d6663b02229de802def13328
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-30 00:30:11 -0600

    Add files via upload

---
+++ EVA3_8_FOR_EACH/src/main/java/com/mycompany/eva3_8_for_each/EVA3_8_FOR_EACH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_8_for_each;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA3_8_FOR_EACH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
