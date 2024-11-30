64b27fa008f44847359d1ef1fc282d88a5f116f1
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-30 00:20:28 -0600

    Add files via upload

---
+++ EVA3_3_CAPTURA/src/main/java/com/mycompany/eva3_3_captura/EVA3_3_CAPTURA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_3_captura;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA3_3_CAPTURA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
