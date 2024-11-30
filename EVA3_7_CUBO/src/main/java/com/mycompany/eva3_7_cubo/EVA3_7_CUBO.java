56a0a7818a99b324ee09d95f9b35e7e69d2db09f
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-30 00:28:18 -0600

    Add files via upload

---
+++ EVA3_7_CUBO/src/main/java/com/mycompany/eva3_7_cubo/EVA3_7_CUBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_7_cubo;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class EVA3_7_CUBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
