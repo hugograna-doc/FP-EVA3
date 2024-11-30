5161612c3502c69ec4009f960ba63c1d2d48fe7f
hugograna-doc <L24550354@chihuahua2.tecnm.mx>
2024-11-30 00:25:50 -0600

    Add files via upload

---
+++ eva3_5_examen_2da/src/main/java/com/mycompany/eva3_5_examen_2da/Eva3_5_examen_2da.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_5_examen_2da;
+
+/**
+ *
+ * @author Un pendejo
+ */
+public class Eva3_5_examen_2da {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
